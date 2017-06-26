f08e82e2c57868b01f926276f901f9bee91b619c
miyuhann <tennkunn96@gmail.com>
2017-06-26 18:18:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,7 +8,7 @@ var HP=100;
 while(0 < HP){
   var li = document.createElement('li');
   var damage = Math.floor(Math.random() * 100);
-  HP -= damege;
+  HP -= damage;
   HP = (HP < 0) ? 0: HP;
   li.textContent = damege + 'のダメージを受けた。残りのHP:' + HP;
   document.getElementById('while').appendChild(li);
