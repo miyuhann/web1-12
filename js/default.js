72de18dee4c301ae4f642d0739c8378504a5ab30
miyuhann <tennkunn96@gmail.com>
2017-06-26 18:13:16 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
 for(var i = 1; i < 10; i++){
-var li = docment.createElement('li'); 
+var li = document.createElement('li'); 
 li.textContent = i + '回目';
 document.getElementById('list').appendChild(li);
 }
