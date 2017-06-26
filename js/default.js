a4891ea386543424fd92008f33efc6446510c213
miyuhann <tennkunn96@gmail.com>
2017-06-26 17:09:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,5 @@
-var li = docment.createElement('li'); js/default.js
+for(var i = 1; i < 10; i++){
+var li = docment.createElement('li'); 
 li.textContent = 'アイテム';
 document.getElementById('list').appendChild(li);
+}
